d3088733201a6a3a4053ad9dcfc851899a38e096
JamesLDavies <34004105+JamesLDavies@users.noreply.github.com>
2021-01-27 17:14:19 +0000

    Added options to choose test data proportion, ML model and to input values for prediction

--- xmas_hack.py
+++ xmas_hack.py
@@ -29,7 +29,6 @@ import streamlit as st
 
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report
@@ -49,8 +48,8 @@ st.set_page_config(
         layout='wide'
         )
     
-st.sidebar.title('James is cool')
-st.sidebar.write('Hello World!')
+st.sidebar.title('James Davies Naughty or Nice ML Dashboard')
+st.sidebar.write('This is a Machine Learning dashboard')
 
 
 def _load_data(path):
@@ -64,22 +63,6 @@ def _load_data(path):
         pandas dataframe: 
     """
     return pd.read_csv(path, header=0)
-
-
-def _find_distinct_values(df):
-    """
-    Create a dict of unique values in each col in df
-    
-    Args:
-        df
-        
-    Returns:
-        distinct_dict
-    """
-    distinct_dict = {}
-    for col in df.columns:
-        distinct_dict[col] = df[col].unique()
-    return distinct_dict
 
 
 def _scaling(series_name):
@@ -98,7 +81,7 @@ def _scaling(series_name):
     return sc_transform
   
 
-def _train_test_split(df):
+def _train_test_split(df, target_value):
     """
     _train_test_split
     
@@ -111,13 +94,19 @@ def _train_test_split(df):
         y_train
         y_test
     """
-    target_value = "target"
+    test_size = st.sidebar.slider(
+        label='Select the proportion of the input data to use for testing',
+        min_value=0.05,
+        max_value=0.95,
+        step=0.05,
+        value=0.3
+        )
     feature_names = df.columns[np.where(df.columns != target_value)]
     X = df[feature_names].values
     y = df[target_value].values.ravel()
 
     (X_train, X_test,
-     y_train, y_test) = train_test_split(X, y, test_size = 0.3,
+     y_train, y_test) = train_test_split(X, y, test_size=test_size,
                                          random_state=42)
     
     return X_train, X_test, y_train, y_test
@@ -232,59 +221,101 @@ def run(path, verbose=0):
                                       options=sorted(raw_df.columns))
     
     _visualise_features_to_behaviour(raw_df, target_col)
-    
-    if verbose == 1:
-      print("Input Data:")
-      print(raw_df.head())
-      distinct_dict = _find_distinct_values(raw_df)
-      print(distinct_dict)
        
     df = raw_df
-   
     
     # train and test split
     (X_train, X_test,
-     y_train, y_test) =  _train_test_split(df)
+     y_train, y_test) =  _train_test_split(df, target_col)
     
     
     # scaling
     X_train = _scaling(X_train)
-    X_test = _scaling(X_test)  
-  
-    clf = _k_neighbor_model(df, X_train, y_train, X_test, y_test)
+    X_test = _scaling(X_test)
     
-    # DecisionTreeClassifier
-    clf = DecisionTreeClassifier().fit(X_train, y_train)
+    # Choose model
+    # TODO: Add arg functions
+    model_dict = {
+        'KNeighborsClassifier': {
+            'model': KNeighborsClassifier,
+            'args': None
+            },
+        'DecisionTreeClassifier': {
+            'model': DecisionTreeClassifier,
+            'args': None
+            },
+        'LogisticRegression': {
+            'model': LogisticRegression,
+            'args': None
+            },
+        'GaussianNB': {
+            'model': GaussianNB,
+            'args': None
+            },
+        'SVC': {
+            'model': SVC,
+            'args': None
+            },
+        'MLPClassifier': {
+            'model': MLPClassifier,
+            'args': None
+            }
+        }
+        
+    model_choice = st.sidebar.selectbox(
+        'Please choose the ML model you would like to use:',
+        sorted(model_dict.keys())
+        )
     
-    # LogisticRegression
-    clf = LogisticRegression().fit(X_train, y_train)
-    
-    # GaussianNB
-    clf = GaussianNB().fit(X_train, y_train)
-    print("GaussianNB")
-    print(clf.score(X_test, y_test))
-    print(classification_report(y_test, clf.predict(X_test)))
-    
-    
-    # SVC
-    clf = SVC().fit(X_train, y_train)
-    print("SVC")
-    print(clf.score(X_test, y_test))
-    print(classification_report(y_test, clf.predict(X_test)))
-    
-    # MLP classifier
-    clf = _MLP_classifier(X_train, y_train)
+    clf = model_dict[model_choice]['model']().fit(X_train, y_train)
     
     clf.predict_proba(X_test[:1])
     clf.predict(X_test[:5, :])
     
     score = clf.score(X_test, y_test)
-    
     report = classification_report(y_test, clf.predict(X_test))
     
+    st.write(f'Score: {score}')
+    st.text('Report: \n' + report)
+    
+    # TODO: Add this to the dashboard
     # Predict whether someone is naughty or nice
     test = [['blue', 'human', 'red', 'tall', 'alive'],
             ['brown', 'human', 'bald', 'tall', 'alive']]
+    
+    # TODO: Write a function to find unique vals and put this in a dict
+    test_data = {}
+    if target_col != 'target':
+        target = st.selectbox('target', ['naughty', 'nice'])
+        test_data['target'] = target
+        
+    if target_col != 'eye_colour':
+        eye_colour = st.selectbox('eye_colour', ['blue', 'green', 'brown'])
+        test_data['eye_colour'] = eye_colour
+        
+    if target_col != 'race':
+        race = st.selectbox('race', ['human', 'android', 'mutant'])
+        test_data['race'] = race
+        
+    if target_col != 'hair_colour':
+        hair_colour = st.selectbox('hair_colour', ['bald', 'black', 'blonde', 'grey'])
+        test_data['hair_colour'] = hair_colour
+        
+    if target_col != 'height':
+        height = st.selectbox('height', ['tall', 'medium', 'small'])
+        test_data['height'] = height
+        
+    if target_col != 'status':
+        status = st.selectbox('status', ['alive', 'dead'])
+        test_data['status'] = status
+        
+    st.write(f"TEST DATA: {test_data}")
+    
+    test_data_2 = [list(test_data.values()),
+                   ['brown', 'human', 'bald', 'tall', 'alive']]
+    prediction = _predict(clf, test_data_2)
+    st.write(f"PREDICTION: {prediction}")
+        
     y_pred = _predict(clf, test)
     
     
